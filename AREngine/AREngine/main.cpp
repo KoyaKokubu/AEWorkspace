3cf681d12ef846c40633991a8421a976a3137c43
Koya <meinaka_koya_06@icloud.com>
2024-04-02 11:40:36 -0700

    Initial commit

---
+++ AREngine/AREngine/main.cpp
@@ -0,0 +1,44 @@
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+#include <iostream>
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+int main()
+{
+	// Vulkan part
+	glfwInit();
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+	uint32_t extensionCount = 0;
+	vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+	std::cout << extensionCount << " extensions supported" << std::endl;
+	glm::mat4 matrix;
+	glm::vec4 vec;
+	auto test = matrix * vec;
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+	}
+	glfwDestroyWindow(window);
+	glfwTerminate();
+
+	// OpenCV part
+	const char* windowName = "OpenCV window";
+
+	cv::Mat img = cv::imread("C:\\Users\\meina\\Pictures\\ReadyPlayerMe-Avatar.jpeg");
+	if (img.data == NULL) {
+		printf("file read error");
+	}
+	else {
+		cv::imshow(windowName, img);
+	}
+
+	cv::waitKey(0);
+
+	return 0;
+}
